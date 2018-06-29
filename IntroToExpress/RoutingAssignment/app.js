74135f5dc7243fc0ba718088df6bb62470d0e127
Lisa <lsr488+cloud9@gmail.com>
2018-06-29 23:59:53 +0000

    Refactored to make sounds an object.

--- IntroToExpress/RoutingAssignment/app.js
+++ IntroToExpress/RoutingAssignment/app.js
@@ -27,27 +27,28 @@
 var express = require("express");
 var app = express(); 
 
+// ROOT ROUTE
 app.get("/", function(req, res) {
-  res.send("Go to /speak/[Animal] OR /repeat/[Word]/[#]");
+  res.send("Go to /speak/[ANIMAL] OR /repeat/[WORD]/[#]");
 });
 
 // ANIMAL ROUTES
 app.get("/speak/:animalName", function(req, res) {
-  var animal = req.params.animalName;
-  function animalNoise() {
-     if(req.params.animalName == "dog") {return "WOOF";}
-     if(req.params.animalName == "pig") {return "OINK";}
-     if(req.params.animalName == "cow") {return "MOO";}
-     if(req.params.animalName == "cat") {return "MEOW";}
-     if(req.params.animalName == "mouse") {return "SQUEAK";}
+  var animal = req.params.animalName.toLowerCase();
+  var sounds = {
+    dog: "WOOF",
+    pig: "OINK",
+    cow: "MOO",
+    cat: "I hate you, human",
+    mouse: "SQUEAK"
   }
+  var sound = sounds[animal];
   
-  res.send("The " + animal + " says " + animalNoise() + ".");
+  res.send('The ' + animal + ' says, "' + sound + '."');
 });
 
 // REPEAT
 app.get("/repeat/:wordToRepeat/:numToRepeat", function(req, res) {
-  console.log(req.params);
   var word = req.params.wordToRepeat;
   var num = parseInt(req.params.numToRepeat);
   var wordString = "";
@@ -66,7 +67,7 @@ app.get("*", function(req, res){
   res.send("Sorry, page not found");
 });
 
-// Tell Express to listen for requests (star server)
+// Tell Express to listen for requests (starT server)
   // "process.env.PORT" and "process.env.IP" arecle specific for cloud9
 app.listen(process.env.PORT, process.env.IP, function() {
   console.log("Server has started.");
