0e36a72b8ac8c13d21784400e773983b5a4e1614
Lisa <lsr488+cloud9@gmail.com>
2018-06-29 22:37:30 +0000

    Learning route parameterss

--- IntroToExpress/FirstExpressApp/app.js
+++ IntroToExpress/FirstExpressApp/app.js
@@ -18,6 +18,23 @@ app.get("/dog", function(req, res) {
   res.send("Meow!");
 });
 
+// goes to subreddit landing page
+app.get("/r/:subredditName", function(req, res){
+  console.log(req.params);
+  var subreddit = req.params.subredditName;
+  res.send("welcome to the " + subreddit.toUpperCase() + " subreddit!");
+});
+
+// goes to an individual post within a subreddit
+app.get("/r/:subredditName/comments/:id/:title", function(req, res) {
+  console.log(req.params);
+  res.send("welcome to the comments page");
+});
+
+app.get("*", function(req, res){
+  res.send("You're a STAR!!");
+});
+
 // Tell Express to listen for requests (star server)
   // "process.env.PORT" and "process.env.IP" arecle specific for cloud9
 app.listen(process.env.PORT, process.env.IP, function() {
