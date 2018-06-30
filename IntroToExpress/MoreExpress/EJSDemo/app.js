9ee4285fc0ba18ea36f94b05f4c7754f766c347a
Lisa <lsr488+cloud9@gmail.com>
2018-06-30 02:33:56 +0000

    Created header and footer partials, added stylesheet.

--- IntroToExpress/MoreExpress/EJSDemo/app.js
+++ IntroToExpress/MoreExpress/EJSDemo/app.js
@@ -1,13 +1,19 @@
 var express = require("express");
 var app = express();
 
+// serves the css
+app.use(express.static("public"));
+
+// lets you remove ".ejs" from the render statement; it assumes .ejs
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res) {
-  res.render("index.ejs");
+  res.render("index");
 });
 
 app.get("/fallinlovewith/:thing", function(req, res) {
   var thing = req.params.thing;
-  res.render("love.ejs", {thingVar: thing});
+  res.render("love", {thingVar: thing});
 });
 
 app.get("/posts", function(req, res) {
@@ -17,7 +23,7 @@ app.get("/posts", function(req, res) {
     {title: "Can you believe this pomsky?", author: "Ian Malcolm"},  
   ];
   //                        templateName: appjsName
-  res.render("posts.ejs", {posts: posts})
+  res.render("posts", {posts: posts})
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
