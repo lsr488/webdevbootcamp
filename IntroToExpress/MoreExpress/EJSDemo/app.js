a998555fa2bc60b61393fc92b383956236c90faf
Lisa <lsr488+cloud9@gmail.com>
2018-06-30 02:11:19 +0000

    Added logic to EJS template

--- IntroToExpress/MoreExpress/EJSDemo/app.js
+++ IntroToExpress/MoreExpress/EJSDemo/app.js
@@ -10,6 +10,16 @@ app.get("/fallinlovewith/:thing", function(req, res) {
   res.render("love.ejs", {thingVar: thing});
 });
 
+app.get("/posts", function(req, res) {
+  var posts = [
+    {title: "Post 1", author: "Suzie"},  
+    {title: "My adorable pet bunny", author: "Alan"},  
+    {title: "Can you believe this pomsky?", author: "Ian Malcolm"},  
+  ];
+  //                        templateName: appjsName
+  res.render("posts.ejs", {posts: posts})
+});
+
 app.listen(process.env.PORT, process.env.IP, function(){
   console.log("Server has started.")
 })
