424cb19249ec2bc613bda154bc7b5c02b78fa9f2
Lisa <lsr488+cloud9@gmail.com>
2018-07-01 17:42:45 +0000

    Search form created, linked to results; dynamic URL created.

--- APIs/movie_search_app/app.js
+++ APIs/movie_search_app/app.js
@@ -3,15 +3,16 @@ var app = express();
 var request = require("request");
 app.set("view engine", "ejs");
 
-// root route
-
-
-// search route
-
+// root route, the search form
+app.get("/", function(req, res) {
+  res.render("search");
+})
 
 // results route
 app.get("/results", function(req, res) {
-  request("http://www.omdbapi.com/?apikey=thewdb&s=new", function(error, response, body) {
+  var query = req.query.search;
+  var url = "http://www.omdbapi.com/?apikey=thewdb&s=" + query;
+  request(url, function(error, response, body) {
      if(!error && response.statusCode == 200) {
        var data = JSON.parse(body);
        res.render("results", {data: data});
