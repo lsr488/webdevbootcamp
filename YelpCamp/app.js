e6baeadebc84b781129974420f0d9f8060a55677
Lisa <lsr488+cloud9@gmail.com>
2018-07-01 18:49:56 +0000

    Add new campgrounds to the list.

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -5,6 +5,11 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+var campgrounds = [
+  {name: "Salmon Creek", image: "https://pixabay.com/get/e834b5062cf4033ed1584d05fb1d4e97e07ee3d21cac104496f0c57aa7eab7b0_340.jpg"},
+  {name: "Granite Hill", image: "https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104496f0c57aa7eab7b0_340.jpg"},
+  {name: "Mountain Goat's Rest", image: "https://pixabay.com/get/e83db50a21f4073ed1584d05fb1d4e97e07ee3d21cac104496f0c57aa7eab7b0_340.jpg"}
+]
 
 // get landing page
 app.get("/", function(req, res) {
@@ -13,22 +18,25 @@ app.get("/", function(req, res) {
 
 // get campgrounds page
 app.get("/campgrounds", function(req, res) {
-  var campgrounds = [
-    {name: "Salmon Creek", image: "https://pixabay.com/get/e834b5062cf4033ed1584d05fb1d4e97e07ee3d21cac104496f0c57aa7eab7b0_340.jpg"},
-    {name: "Granite Hill", image: "https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104496f0c57aa7eab7b0_340.jpg"},
-    {name: "Mountain Goat's Rest", image: "https://pixabay.com/get/e83db50a21f4073ed1584d05fb1d4e97e07ee3d21cac104496f0c57aa7eab7b0_340.jpg"}
-  ]
-  
   res.render("campgrounds", {campgrounds: campgrounds});
 });
 
-// post campgrounds
+// add new campgrounds
 app.post("/campgrounds", function(req, res) {
   // get data from form
+  var name = req.body.name;
+  var image = req.body.image;
+  var newCampground = {name: name, image: image}
   // add to campgrounds array
+  campgrounds.push(newCampground);
   // redirect back to campgrounds page
+  res.redirect("/campgrounds");
 });
 
+// show new campgrounds form
+app.get("/campgrounds/new", function(req, res) {
+  res.render("new.ejs")
+});
 
 app.listen(process.env.PORT, process.env.IP, function() {
   console.log("YelpCamp server has started.");
