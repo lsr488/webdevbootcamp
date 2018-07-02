067fd206125f6188b7c7df0974aaf6efc6b95c0f
Lisa <lsr488+cloud9@gmail.com>
2018-07-02 03:38:55 +0000

    Added express-sanitizer. Further CSS tweaks.

--- RESTfulRouting/RESTfulBlogApp/app.js
+++ RESTfulRouting/RESTfulBlogApp/app.js
@@ -2,7 +2,8 @@ var express = require("express"),
     app = express(),
     bodyParser = require("body-parser"),
     mongoose = require("mongoose"),
-    methodOverride = require("method-override");
+    methodOverride = require("method-override"),
+    expressSanitizer = require("express-sanitizer");
     
 // app config    
 mongoose.connect("mongodb://localhost/restful_blog_app");
@@ -11,6 +12,7 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
+app.use(expressSanitizer());
 
 // mongoose/model config
 var blogSchema = new mongoose.Schema({
@@ -33,7 +35,7 @@ app.get("/", function(req, res) {
 app.get("/blogs", function(req, res) {
   Blog.find({}, function(error, blogs) {
     if(error) {
-      console.log("ERROR");
+      console.log("Error in the INDEX route.");
     } else {
       res.render("index", {blogs: blogs});
     }
@@ -47,8 +49,10 @@ app.get("/blogs/new", function(req, res) {
 
 // CREATE ROUTE
 app.post("/blogs", function(req, res) {
+  req.body.blog.body = req.sanitize(req.body.blog.body);
   Blog.create(req.body.blog, function(error, newBlog) {
     if(error) {
+      console.log("Error in the CREATE route.");
       res.render("new");
     } else {
       res.redirect("/blogs");
@@ -60,6 +64,7 @@ app.post("/blogs", function(req, res) {
 app.get("/blogs/:id", function(req, res) {
   Blog.findById(req.params.id, function(err, foundBlog) {
     if(err) {
+      console.log("Error in the SHOW route.");
       res.redirect("/blogs");
     } else {
       res.render("show", {blog: foundBlog});
@@ -72,6 +77,7 @@ app.get("/blogs/:id", function(req, res) {
 app.get("/blogs/:id/edit", function(req, res) {
    Blog.findById(req.params.id, function(error, foundBlog) {
      if(error) {
+      console.log("Error in the EDIT route.");
        res.redirect("/blogs");
      } else {
         res.render("edit", {blog: foundBlog}); 
@@ -81,8 +87,10 @@ app.get("/blogs/:id/edit", function(req, res) {
 
 // UPDATE ROUTE
 app.put("/blogs/:id", function(req, res) {
+  req.body.blog.body = req.sanitize(req.body.blog.body);
   Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(error, updatedBlog) {
     if(error) {
+      console.log("Error in the UPDATE route.");
       res.redirect("/blogs");
     } else {
       res.redirect("/blogs/" + req.params.id);
@@ -94,6 +102,7 @@ app.put("/blogs/:id", function(req, res) {
 app.delete("/blogs/:id", function(req, res) {
   Blog.findByIdAndRemove(req.params.id, function(error) {
     if(error) {
+      console.log("Error in the DESTROY route.");
       res.redirect("/blogs");
     } else {
       res.redirect("/blogs");
