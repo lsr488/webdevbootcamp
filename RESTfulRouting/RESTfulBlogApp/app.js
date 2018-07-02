442bd0bdcbff566d57e9d2d90b1f47eee7ab706e
Lisa <lsr488+cloud9@gmail.com>
2018-07-02 00:24:50 +0000

    Blog app to practice REST principles

---
+++ RESTfulRouting/RESTfulBlogApp/app.js
@@ -0,0 +1,54 @@
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose");
+    
+// app config    
+mongoose.connect("mongodb://localhost/restful_blog_app");
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+// mongoose/model config
+var blogSchema = new mongoose.Schema({
+  title: String,
+  image: String,
+  body: String,
+  created: {type: Date, default: Date.now}
+});
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+// RESTful routes
+
+// INDEX ROUTES
+app.get("/", function(req, res) {
+  res.redirect("blogs");
+})
+
+app.get("/blogs", function(req, res) {
+  Blog.find({}, function(error, blogs) {
+    if(error) {
+      console.log("ERROR");
+    } else {
+      res.render("index", {blogs: blogs});
+    }
+  });
+});
+
+// NEW ROUTE
+
+// CREATE ROUTE
+
+// SHOW ROUTE
+
+// EDIT ROUTE
+
+// UPDATE ROUTE
+
+// DESTROY ROUTE
+
+app.listen(process.env.PORT, process.env.IP, function() {
+  console.log("Server has started.");
+});
