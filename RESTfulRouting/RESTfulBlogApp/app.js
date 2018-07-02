36eb89f39053b013bb6b65c616edf2aad9fd8b25
Lisa <lsr488+cloud9@gmail.com>
2018-07-02 01:28:53 +0000

    Created SHOW template, added styling.

--- RESTfulRouting/RESTfulBlogApp/app.js
+++ RESTfulRouting/RESTfulBlogApp/app.js
@@ -55,6 +55,15 @@ app.post("/blogs", function(req, res) {
 });
 
 // SHOW ROUTE
+app.get("/blogs/:id", function(req, res) {
+  Blog.findById(req.params.id, function(err, foundBlog) {
+    if(err) {
+      res.redirect("/blogs");
+    } else {
+      res.render("show", {blog: foundBlog});
+    }
+  });
+});
 
 // EDIT ROUTE
 
