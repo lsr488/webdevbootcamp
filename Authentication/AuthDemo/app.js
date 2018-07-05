04a0b3a94cddbc8a4f0464d7ba1495117fdd7c52
Lisa <lsr488+cloud9@gmail.com>
2018-07-05 00:36:43 +0000

    Passport, PassportLocal, PassportLocalMongoose set up

--- Authentication/AuthDemo/app.js
+++ Authentication/AuthDemo/app.js
@@ -1,11 +1,30 @@
-var express = require("express");
-var mongoose = require("mongoose");
+var express               = require("express"),
+    bodyParser            = require("body-parser"),
+    passport              = require("passport"),
+    LocalStrategy         = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose"),
+    User                  = require("./models/user"),
+    mongoose              = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/auth_demo_app");
+
+mongoose.connect("mongodb://localhost:27017/auth_demo_app", { useNewUrlParser: true });
 
 var app = express();
-
 app.set("view engine", "ejs");
+
+app.use(require("express-session")({
+  // 'secret' string is used to code and unencode information in a session
+  secret: "Rusty is the best and cutest dog in the world",
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// very import for passport
+// responsible for reading session, encoding and decoding info if logged in or not
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res) {
   res.render("home");
